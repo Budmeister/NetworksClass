35be0a315f8c77f4fe14a47967779117ef4d49e2
Brian Smith <brian.d.smith10@gmail.com>
2023-09-05 21:44:22 -0500

    Socket2_UDP_Pinger

--- Socket2_UDP_Pinger/UDPPingerClient.py
+++ Socket2_UDP_Pinger/UDPPingerClient.py
@@ -16,9 +16,9 @@ def green(msg):
 def get_message(n):
     return f"Ping {n} {datetime.now()}".encode()
 
-def ping(n, clientSocket, mintime, maxtime, successes, failures):
+def ping(n, address, clientSocket, mintime, maxtime, successes, failures):
     message = get_message(n)
-    address = ("localhost", 12000)
+    # address = ("localhost", 12000)
     print(f"Sending ping {n}")
     start = time()
     clientSocket.sendto(message, address)
@@ -41,6 +41,11 @@ def ping(n, clientSocket, mintime, maxtime, successes, failures):
     return mintime, maxtime, successes, failures
 
 def main():
+    ip = input("IP: ")
+    port = input("Port: ")
+    address = (ip, int(port))
+    print(f"Pinging {ip}:{port} with 10 pings")
+
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.settimeout(1)
     # clientSocket.bind(('', 12000))
@@ -51,7 +56,7 @@ def main():
     failures = 0
 
     for n in range(10):
-        mintime, maxtime, successes, failures = ping(n, clientSocket, mintime, maxtime, successes, failures)
+        mintime, maxtime, successes, failures = ping(n, address, clientSocket, mintime, maxtime, successes, failures)
     
     print()
     print(f"Successes: {successes}")
